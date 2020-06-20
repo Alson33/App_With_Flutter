7032766632a1811957d9d5a02640480d75f48f2c
Alson33 <be2018se607@gces.edu.np>
2020-06-20 10:58:35 +0545

    made strech pages

---
+++ HealthyApp/lib/strechPages/strechDetailPage.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+// TODO: Lots of work to do here
+class StrechDetailPage extends StatelessWidget {
+  const StrechDetailPage({
+    Key key,
+    @required this.image,
+    @required this.tag,
+  }) : super(key: key);
+
+  final AssetImage image;
+  final String tag;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    width: double.infinity,
+                    height: 60.0,
+                    color: Colors.white60,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.arrow_back,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 300,
+                    child: Hero(
+                      tag: tag,
+                      child: Image(
+                        image: image,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ HealthyApp/lib/strechPages/strechPage.dart
@@ -0,0 +1,114 @@
+import 'package:HealthyApp/widgets/repeatedWidget.dart';
+import 'package:flutter/material.dart';
+import './strechDetailPage.dart';
+// import './listOfRecipe.dart';
+
+// ListOfRecipe listOfRecipe;
+
+class StrechPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var _streches = [
+      {
+        'image': AssetImage('assets/images/exeIcon.jpg'),
+        'tags': 'image 11',
+        'steps': ['S1 one', 'S1 two', 'S1 three'],
+      },
+      {
+        'image': AssetImage('assets/images/vegeIcon.jpg'),
+        'tags': 'image 21',
+        'steps': ['S2 one', 'S2 two', 'S2 three'],
+      },
+      {
+        'image': AssetImage('assets/images/vegeIcon.jpg'),
+        'tags': 'image 31',
+        'steps': ['S3 one', 'S3 two', 'S3 three'],
+      },
+      {
+        'image': AssetImage('assets/images/exeIcon.jpg'),
+        'tags': 'image 41',
+        'steps': ['S4 one', 'S4 two', 'S4 three'],
+      },
+      {
+        'image': AssetImage('assets/images/exeIcon.jpg'),
+        'tags': 'image 51',
+        'steps': ['S5 one', 'S5 two', 'S5 three'],
+      },
+    ];
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          // TODO: Make a custom paint for strech page
+          // CustomPaint(),
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.transparent,
+          ),
+          Column(
+            children: <Widget>[
+              BarWidget(isSettingPage: false),
+              TitleWidget(title: 'Streches', color: Colors.black),
+              Expanded(
+                flex: 6,
+                child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      margin: EdgeInsets.all(20.0),
+                      color: Colors.grey[100],
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 2,
+                            child: GestureDetector(
+                              child: Hero(
+                                // FIXME: Give appropriate tag and actual dish arts
+                                tag: _streches[index]['tags'],
+                                child: Image(
+                                  image: _streches[index]['image'],
+                                  fit: BoxFit.cover,
+                                ),
+                                // TODO: Make dish arts
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => StrechDetailPage(
+                                      image: _streches[index]['image'],
+                                      tag: _streches[index]['tags'],
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Column(
+                              children: <Widget>[
+                                // FIXME: Make attractive
+                                Text('Name of the strech.'),
+                                Text('Infos....'),
+                                FlatButton(
+                                  onPressed: () {},
+                                  child: Text('Strech'),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
