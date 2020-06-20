ec3675b666f63ef4df77ca1d1fd44bffb0784ee1
Alson33 <be2018se607@gces.edu.np>
2020-06-20 10:59:41 +0545

    made folder for recipe page and made it functional

--- HealthyApp/lib/recipePages/recipeDetailPage.dart
+++ HealthyApp/lib/recipePages/recipeDetailPage.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 
 // TODO: Lots of work to do here
 class RecipeDetailPage extends StatelessWidget {
+  const RecipeDetailPage({
+    Key key,
+    @required this.image,
+    @required this.tag,
+  }) : super(key: key);
+
+  final AssetImage image;
+  final String tag;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,19 +24,29 @@ class RecipeDetailPage extends StatelessWidget {
                   Container(
                     width: double.infinity,
                     color: Colors.white60,
-                    child: IconButton(
-                      icon: Icon(Icons.arrow_back),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
+                    height: 60.0,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.arrow_back,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
                     ),
                   ),
                   Container(
                     width: double.infinity,
                     height: 300,
                     child: Hero(
-                      tag: 'firstImage',
-                      child: Text('FirstImage'),
+                      tag: tag,
+                      child: Image(
+                        image: image,
+                        fit: BoxFit.contain,
+                      ),
                     ),
                   ),
                 ],
--- HealthyApp/lib/recipePages/recipePage.dart
+++ HealthyApp/lib/recipePages/recipePage.dart
@@ -1,13 +1,44 @@
 import 'package:HealthyApp/recipePages/recipeDetailPage.dart';
-import 'package:HealthyApp/repeatedWidget.dart';
+import 'package:HealthyApp/widgets/forHomePageWidget.dart';
+import 'package:HealthyApp/widgets/repeatedWidget.dart';
 import 'package:flutter/material.dart';
-import './listOfRecipe.dart';
+// import './listOfRecipe.dart';
 
-ListOfRecipe listOfRecipe;
+// ListOfRecipe listOfRecipe;
 
 class RecipePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var _recipes = [
+      {
+        'image': AssetImage('assets/images/vegeIcon.jpg'),
+        'tags': 'image 1',
+        'steps': ['S1 one', 'S1 two', 'S1 three'],
+      },
+      {
+        'image': AssetImage('assets/images/exeIcon.jpg'),
+        'tags': 'image 2',
+        'steps': ['S2 one', 'S2 two', 'S2 three'],
+      },
+      {
+        'image': AssetImage('assets/images/vegeIcon.jpg'),
+        'tags': 'image 3',
+        'steps': ['S3 one', 'S3 two', 'S3 three'],
+      },
+      {
+        'image': AssetImage('assets/images/exeIcon.jpg'),
+        'tags': 'image 4',
+        'steps': ['S4 one', 'S4 two', 'S4 three'],
+      },
+      {
+        'image': AssetImage('assets/images/vegeIcon.jpg'),
+        'tags': 'image 5',
+        'steps': ['S5 one', 'S5 two', 'S5 three'],
+      },
+    ];
+
+    // List<String> _images = listOfRecipe.giveImage();
+
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -22,54 +53,57 @@ class RecipePage extends StatelessWidget {
             children: <Widget>[
               BarWidget(isSettingPage: false),
               TitleWidget(title: 'Recipes', color: Colors.black),
-              // TODO: Create a list view for the recipies
-              // make hero animation avialable if possible
               Expanded(
                 flex: 6,
-                // TODO: Make the list builder work
                 child: ListView.builder(
-                  itemCount: listOfRecipe.length(),
+                  itemCount: 5,
                   itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Card(
-                        child: Row(
-                          children: <Widget>[
-                            Expanded(
-                              flex: 2,
-                              child: GestureDetector(
-                                child: Hero(
-                                  // FIXME: Give appropriate tag and actual images
-                                  tag: listOfRecipe.giveTag(index),
-                                  child: Text(listOfRecipe.giveImage(index)),
-                                  // TODO: Make dish arts
+                    return Card(
+                      margin: EdgeInsets.all(20.0),
+                      color: Colors.grey[100],
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 2,
+                            child: GestureDetector(
+                              child: Hero(
+                                // FIXME: Give appropriate tag and actual dish arts
+                                tag: _recipes[index]['tags'],
+                                child: Image(
+                                  image: _recipes[index]['image'],
+                                  fit: BoxFit.cover,
                                 ),
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => RecipeDetailPage(),
+                                // TODO: Make dish arts
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => RecipeDetailPage(
+                                      image: _recipes[index]['image'],
+                                      tag: _recipes[index]['tags'],
                                     ),
-                                  );
-                                },
-                              ),
-                            ),
-                            Expanded(
-                              flex: 4,
-                              child: Column(
-                                children: <Widget>[
-                                  // FIXME: Make attractive
-                                  Text('Name of the recipe.'),
-                                  Text('Infos....'),
-                                  FlatButton(
-                                    onPressed: () {},
-                                    child: Text('Learn'),
                                   ),
-                                ],
-                              ),
+                                );
+                              },
                             ),
-                          ],
-                        ),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Column(
+                              children: <Widget>[
+                                // FIXME: Make attractive
+                                // Text('Name of the recipe.'),
+                                SmallTitleWidget(title: 'Name', color: Colors.black, align: Alignment.centerLeft),
+                                // DescriptionPart(title: 'Name', descp: 'Infos...................', buttonTitle: 'Learn',),
+                                FlatButton(
+                                  onPressed: () {},
+                                  child: Text('Learn'),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     );
                   },
