dfc5619d9b146845e1b23d9fbeb8e9864cd7e8ae
Alson33 <garbuja12alson@gmail.com>
2020-10-20 14:59:13 +0545

    new layout with bottomsheet

--- HealthyApp/lib/main.dart
+++ HealthyApp/lib/main.dart
@@ -20,32 +20,32 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: <Widget>[
-          // FIXME: Try to make animated transition
-          CustomPaint(
-            painter: UpperSmallCircle(),
-            size: Size.fromHeight(200),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: CustomPaint(
-              painter: LowerBigCircle(),
-              size: Size.fromHeight(370),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: <Widget>[
+            // FIXME: Try to make animated transition
+            CustomPaint(
+              painter: UpperSmallCircle(),
+              size: Size.fromHeight(200),
             ),
-          ),
-          Column(
-            children: <Widget>[
-              BarWidget(isSettingPage: false),
-              TitleWidget(title: 'Drink', color: Colors.white),
-              HomePageMainPart(),
-              EatHealthyWidget(),
-              BeHealthyWidget(),
-            ],
-          ),
-        ],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: CustomPaint(
+                painter: LowerBigCircle(),
+                size: Size.fromHeight(370),
+              ),
+            ),
+            Column(
+              children: <Widget>[
+                BarWidget(isSettingPage: false),
+                TitleWidget(title: 'Drink', color: Colors.white),
+                HomePageMainPart(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -61,7 +61,7 @@ class HomePageMainPart extends StatefulWidget {
 }
 
 class _HomePageMainPartState extends State<HomePageMainPart> {
-  bool _isBottle = false;
+  bool _isBottle = true;
   ImageIcon icon;
   int _count = 1;
   int _countHour = 1;
@@ -89,10 +89,12 @@ class _HomePageMainPartState extends State<HomePageMainPart> {
       flex: 5,
       child: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Expanded(
               flex: 4,
               child: Container(
+                alignment: Alignment.topCenter,
                 child: Row(
                   children: <Widget>[
                     for (int i = 1; i <= _count; i++)
@@ -104,228 +106,242 @@ class _HomePageMainPartState extends State<HomePageMainPart> {
               ),
             ),
             Expanded(
-              flex: 2,
-              child: Table(
-                children: <TableRow>[
-                  TableRow(
-                    children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          // FIXME: Make attractive
-                          Expanded(
-                            child: IconButton(
-                                icon: Icon(
-                                  Icons.remove,
-                                  color: Colors.black,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    if (_isBottle) {
-                                      if (_count <= 1) {
-                                        _count = 1;
-                                      } else {
-                                        _count--;
-                                      }
-                                    } else {
-                                      if (_count <= 4) {
-                                        _count = 4;
-                                        Scaffold.of(context).showSnackBar(
-                                          SnackBar(
-                                            content: Text(
-                                                'At least 4 glass of water is needed'),
-                                            backgroundColor: Colors.red,
-                                          ),
-                                        );
-                                      } else {
-                                        _count--;
-                                      }
-                                    }
-                                  });
-                                }),
-                          ),
-                          Expanded(
-                            child: Text('$_count'),
-                          ),
-                          Expanded(
-                            child: IconButton(
-                                icon: Icon(
-                                  Icons.add,
-                                  color: Colors.black,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    if (_isBottle) {
-                                      if (_count >= 5) {
-                                        _count = 5;
-                                        // FIXME: Try to experiment with options like flushbar or alert too
-                                        Scaffold.of(context).showSnackBar(
-                                          SnackBar(
-                                            content: Text(
-                                                'More than $_count Bottle per day is not good for heath'),
-                                            backgroundColor: Colors.red,
-                                          ),
-                                        );
-                                      } else {
-                                        _count++;
-                                      }
-                                    } else {
-                                      if (_count >= 20) {
-                                        _count = 20;
-                                        // FIXME: Try to experiment with options like flushbar or alert too
-                                        Scaffold.of(context).showSnackBar(
-                                          SnackBar(
-                                            content: Text(
-                                                'More than $_count Glass per day is not good for heath'),
-                                            backgroundColor: Colors.red,
-                                          ),
-                                        );
-                                      } else {
-                                        _count++;
-                                      }
-                                    }
-                                  });
-                                }),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          // FIXME: Make attractive
-                          Expanded(
-                            child: IconButton(
-                                icon: Icon(
-                                  Icons.remove,
-                                  color: Colors.black,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    if (_countHour <= 1) {
-                                      _countHour = 1;
-                                    } else {
-                                      _countHour--;
-                                    }
-                                  });
-                                }),
-                          ),
-                          Expanded(
-                            child: Text('$_countHour'),
-                          ),
-                          Expanded(
-                            child: IconButton(
-                                icon: Icon(
-                                  Icons.add,
-                                  color: Colors.black,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    if (_countHour >= 3) {
-                                      _countHour = 3;
-                                      // FIXME: Try to experiment with options like flushbar or alert too
-                                      Scaffold.of(context).showSnackBar(
-                                        SnackBar(
-                                          content: Text(
-                                              '$_countHour Hour gap is the most for water drinking gap'),
-                                          backgroundColor: Colors.red,
-                                        ),
-                                      );
-                                    } else {
-                                      _countHour++;
-                                    }
-                                  });
-                                }),
-                          ),
-                        ],
-                      ),
-                    ],
+              flex: 1,
+              child: Container(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.grey[100].withOpacity(0.2),
                   ),
-                  TableRow(
-                    // TODO: Make them more attractive and positioned properly
-                    children: <Widget>[
-                      Text('Add Bottle'),
-                      Text('In Hour'),
-                    ],
+                  child: IconButton(
+                    visualDensity: VisualDensity.adaptivePlatformDensity,
+                    padding: EdgeInsets.all(20),
+                    icon: Icon(
+                      Icons.add,
+                    ),
+                    iconSize: 40,
+                    onPressed: () {
+                      _showSettingBottomSheet(context);
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void _showSettingBottomSheet(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext bContext) {
+      return Container(
+        height: MediaQuery.of(context).size.height * 0.45,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Choose a medium:'),
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.white10,
+                    border: Border.all(
+                      color: Colors.black,
+                    ),
+                  ),
+                  width: 60,
+                  height: 60,
+                  child: Image.asset('assets/icons/bottle.ico'),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.white10,
+                    border: Border.all(
+                      color: Colors.black,
+                    ),
+                  ),
+                  width: 60,
+                  height: 60,
+                  child: Image.asset('assets/icons/glass.ico'),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('No of bottles:'),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.only(left: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(5),
+                        bottomLeft: Radius.circular(5),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text('1'),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text('2'),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text('3'),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text('4'),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(5),
+                        bottomRight: Radius.circular(5),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text('5'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Remind me in:'),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.only(left: 10),
+              child: Row(
+                children: [
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(5),
+                        bottomLeft: Radius.circular(5),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text('10min'),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text('30min'),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text('50min'),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text('60min'),
+                    ),
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(5),
+                        bottomRight: Radius.circular(5),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text('90min'),
+                    ),
                   ),
                 ],
               ),
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class BeHealthyWidget extends StatelessWidget {
-  const BeHealthyWidget({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: 2,
-      child: Container(
-        child: Row(
-          children: <Widget>[
-            // FIXME: Make info more attractive
-            Expanded(
-              flex: 5,
-              child: Container(
-                child: Text('Info'),
-              ),
-            ),
-            // FIXME: Make a strech icon instead of text
-            Expanded(
-              flex: 2,
-              child: Container(
-                child: Text('StrechImage'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class EatHealthyWidget extends StatelessWidget {
-  const EatHealthyWidget({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      flex: 2,
-      child: Container(
-        child: Row(
-          children: <Widget>[
-            // FIXME: Make a vege icon instead of text
-            Expanded(
-              flex: 2,
-              child: Container(
-                child: GestureDetector(
-                  child: Text('vegeImage'),
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => RecipePage(),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-            // FIXME: Make info more attractive
-            Expanded(
-              flex: 5,
-              child: Container(
-                child: Text('Info...'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+      );
+    },
+  );
 }
