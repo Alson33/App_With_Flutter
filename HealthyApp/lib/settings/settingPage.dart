2ebfd2887dea840a60343554569f9007894aa94e
Alson33 <garbuja12alson@gmail.com>
2020-10-20 14:59:51 +0545

    added a safearea

--- HealthyApp/lib/settings/settingPage.dart
+++ HealthyApp/lib/settings/settingPage.dart
@@ -5,66 +5,68 @@ import '../customPaints/customPaints.dart';
 class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: <Widget>[
-          CustomPaint(
-            painter: UpperBigCircle(),
-            size: Size.fromHeight(370),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: CustomPaint(
-              painter: LowerSmallCircle(),
-              size: Size.fromHeight(200),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: <Widget>[
+            CustomPaint(
+              painter: UpperBigCircle(),
+              size: Size.fromHeight(370),
             ),
-          ),
-          Column(
-            children: <Widget>[
-              BarWidget(isSettingPage: true),
-              TitleWidget(title: 'Settings', color: Colors.white),
-              Expanded(
-                flex: 4,
-                child: Column(
-                  children: <Widget>[
-                    // TODO: Make IconButtons for the objectiveIcon
-                    SettingPageMainPart(
-                        objectiveText: 'Change the color theme',
-                        objectiveIcon: 'Color picker'),
-                    SettingPageMainPart(
-                        objectiveText: 'Change between bottle and glass',
-                        objectiveIcon: 'Bottle/Glass'),
-                    SettingPageMainPart(
-                        objectiveText: 'Chaneg hour and minute',
-                        objectiveIcon: 'Hr/min'),
-                  ],
+            Align(
+              alignment: Alignment.bottomRight,
+              child: CustomPaint(
+                painter: LowerSmallCircle(),
+                size: Size.fromHeight(200),
+              ),
+            ),
+            Column(
+              children: <Widget>[
+                BarWidget(isSettingPage: true),
+                TitleWidget(title: 'Settings', color: Colors.white),
+                Expanded(
+                  flex: 4,
+                  child: Column(
+                    children: <Widget>[
+                      // TODO: Make IconButtons for the objectiveIcon
+                      SettingPageMainPart(
+                          objectiveText: 'Change the color theme',
+                          objectiveIcon: 'Color picker'),
+                      SettingPageMainPart(
+                          objectiveText: 'Change between bottle and glass',
+                          objectiveIcon: 'Bottle/Glass'),
+                      SettingPageMainPart(
+                          objectiveText: 'Chaneg hour and minute',
+                          objectiveIcon: 'Hr/min'),
+                    ],
+                  ),
                 ),
-              ),
-              TitleWidget(title: 'Graph', color: Colors.black),
-              Expanded(
-                flex: 4,
-                child: Text('Graph'),
-              ),
-              Expanded(
-                flex: 1,
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 20.0,
-                      horizontal: 40.0,
-                    ),
-                    child: Text(
-                      'Growth rate',
-                      style: TextStyle(fontSize: 25.0),
+                TitleWidget(title: 'Graph', color: Colors.black),
+                Expanded(
+                  flex: 4,
+                  child: Text('Graph'),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20.0,
+                        horizontal: 40.0,
+                      ),
+                      child: Text(
+                        'Growth rate',
+                        style: TextStyle(fontSize: 25.0),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
